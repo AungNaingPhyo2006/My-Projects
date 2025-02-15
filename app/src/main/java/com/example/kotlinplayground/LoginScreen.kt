087c84827f66e491d0bc00e6a118b99f6ca181c4
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-02-15 11:11:10 +0630

    Finish Simple Login

--- app/src/main/java/com/example/kotlinplayground/LoginScreen.kt
+++ app/src/main/java/com/example/kotlinplayground/LoginScreen.kt
@@ -1,11 +1,14 @@
 package com.example.kotlinplayground
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,15 +17,23 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
     Column(modifier= modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
@@ -31,22 +42,26 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             text="Welcome Back",
             fontSize = 28.sp, fontWeight = FontWeight.Bold
         )
-        Spacer(modifier= modifier.height(4.dp))
         Text(text="Login to your account!")
-        Spacer(modifier= modifier.height(4.dp))
-        OutlinedTextField(value="", onValueChange = {},
+        OutlinedTextField(value=email, onValueChange = {
+            email = it
+        },
             label = {
                 Text(text="Email Address")
             } )
 
-        OutlinedTextField(value="", onValueChange = {},
+        OutlinedTextField(value=password, onValueChange = {
+            password = it
+        },
             label = {
                 Text(text="Password")
-            } )
+            }, visualTransformation = PasswordVisualTransformation() )
 
-        Spacer(modifier= modifier.height(2.dp))
+//        Spacer(modifier= modifier.height(2.dp))
 
-        Button(onClick = {}) {
+        Button(onClick = {
+            Log.i("Credential", "Email : $email, Password : $password")
+        }) {
             Text(text="Login")
         }
 
@@ -55,6 +70,22 @@ fun LoginScreen(modifier: Modifier = Modifier) {
 //        }
         Text(text="Forgot Password!", modifier = modifier.clickable {  })
         Text(text="Sign in With:", modifier = modifier.clickable {  })
+//        Text(text=email, modifier = modifier.clickable {  })
+
+      Row(modifier = modifier.fillMaxWidth().padding(40.dp), horizontalArrangement = Arrangement.SpaceEvenly) {
+          Image(painter = painterResource(id = R.drawable.mom_image),
+              contentDescription = "Facebook",modifier=modifier.size(60.dp).clickable {
+
+              })
+          Image(painter = painterResource(id = R.drawable.mom_image),
+              contentDescription = "Google",modifier=modifier.size(60.dp).clickable {
+
+              })
+          Image(painter = painterResource(id = R.drawable.mom_image),
+              contentDescription = "Twitter ",modifier=modifier.size(60.dp).clickable {
+
+              })
+      }
 
     }
 
