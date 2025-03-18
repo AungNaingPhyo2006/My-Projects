fc6186ef34ab8d30bcea6f6839e12abe0bdb7486
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-03-19 00:07:49 +0630

    set login and signup screens

--- app/src/main/java/com/example/kotlinplayground/screens/LoginScreen.kt
+++ app/src/main/java/com/example/kotlinplayground/screens/LoginScreen.kt
@@ -1,11 +1,79 @@
 package com.example.kotlinplayground.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.kotlinplayground.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier, navController: NavHostController) {
-    Text(text="Login Screen")
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column (modifier = modifier.fillMaxSize().padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome back!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontFamily  = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Signin to your Account!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 20.sp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
+            modifier = Modifier.fillMaxWidth()
+                .height(200.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email Address") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth().height(50.dp)) {
+            Text(text = "Sign up", fontSize = 22.sp)
+        }
+    }
 }
