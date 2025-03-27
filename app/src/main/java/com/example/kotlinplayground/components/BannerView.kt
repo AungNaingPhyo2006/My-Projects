48af03ffde94e4ecff76800f848ce78cad5d909b
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-03-28 01:56:41 +0630

    add banner 6-24

---
+++ app/src/main/java/com/example/kotlinplayground/components/BannerView.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinplayground.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BannerView(modifier: Modifier = Modifier) {
+    Text(text="Welcome Banner")
+}
--- app/src/main/java/com/example/kotlinplayground/pages/HomePage.kt
+++ app/src/main/java/com/example/kotlinplayground/pages/HomePage.kt
@@ -1,10 +1,21 @@
 package com.example.kotlinplayground.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.kotlinplayground.components.BannerView
+import com.example.kotlinplayground.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Text(text = "HomePage")
+   Column (modifier = Modifier.fillMaxSize()
+       .padding(32.dp)
+   ){
+       HeaderView(modifier)
+       BannerView(modifier)
+   }
 }
