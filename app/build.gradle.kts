174bcf1f3cf9eac1ab061004d64f27d00105607a
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-03-31 21:45:10 +0630

    add header and banner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:$room_version")
     kapt ("androidx.room:room-compiler:$room_version")
+
+    implementation("io.coil-kt:coil-compose:2.2.0")
+    implementation("com.tbuonomo:dotsindicator:5.1.0")
 }
--- app/src/main/java/com/example/kotlinplayground/pages/HomePage.kt
+++ app/src/main/java/com/example/kotlinplayground/pages/HomePage.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinplayground.pages
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,9 +15,10 @@ import com.example.kotlinplayground.components.HeaderView
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
    Column (modifier = Modifier.fillMaxSize()
-       .padding(32.dp)
+       .padding(vertical = 32.dp, horizontal = 16.dp)
    ){
        HeaderView(modifier)
-       BannerView(modifier)
+       Spacer(modifier = Modifier.height(10.dp))
+       BannerView(modifier = Modifier.height(200.dp))
    }
 }
